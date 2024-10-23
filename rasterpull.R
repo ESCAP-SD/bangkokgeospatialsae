34e9998b04d240b0997c07a66dbefa57ec7a8379
Josh Merfeld <74187569+JoshMerfeld@users.noreply.github.com>
2024-10-23 13:26:36 +0700

    rasters

--- rasterpull.R
+++ rasterpull.R
@@ -167,19 +167,20 @@ plot_grid(g1, g2)
 
 # MPC
 library(rstac)
+library(terra)
 library(sf)
 
 # let's use Korea
-kgrid <- read_sf("vectorfilesdata/kgrid.shp")
+kgrid <- vect("vectorfilesdata/kgrid.shp")
 # to lon/lat
-kgrid <- st_transform(kgrid, "EPSG:4326")
-kgridbox <- st_bbox(kgrid)
+kgrid <- project(kgrid, "EPSG:4326")
+kgridbox <- ext(kgrid)
 
 # let's pull some pollution data
 s_obj <- stac("https://planetarycomputer.microsoft.com/api/stac/v1")
 it_obj <- s_obj %>%
   stac_search(collections = "sentinel-5p-l2-netcdf",
-              bbox = kgridbox,
+              bbox = c(kgridbox[1], kgridbox[3], kgridbox[2], kgridbox[4]),
               datetime = "2019-01-01/2019-12-31",
               limit = 1000) %>%
   get_request() %>%
@@ -189,13 +190,13 @@ it_obj
 # let's just download the first one
 url <- paste0("/vsicurl/", it_obj$features[[1]]$assets$so2$href)
 # also get the bounding box for that area
-bbox <- it_obj$features[[1]]$bbox
+bb <- it_obj$features[[1]]$bbox
 # load raster
 rall <- rast(url)
 # keep just the layer we want and transform to array
 rall <- as.array(rall[["aerosol_index_340_380"]])
 # now back to raster with the appropriate extent and CRS
-rall <- rast(rall, crs = "EPSG:4326", extent = bbox[[1]])
+rall <- rast(rall, crs = "EPSG:4326", extent = ext(c(bb[1], bb[3], bb[2], bb[4])))
 
 # extract to grids
 extractpol <- exact_extract(rall, kgrid, fun = "mean", append_cols = "id")
@@ -207,5 +208,40 @@ kgrids <- kgrid |>
 ggplot() +
   geom_spatvector(data = kgrids, aes(fill = so2), color = NA) +
   scale_fill_distiller("SO2", palette = "Spectral")
+
+
+
+
+
+
+
+
+
+
+# let's pull some pollution data
+s_obj <- stac("https://planetarycomputer.microsoft.com/api/stac/v1")
+it_obj <- s_obj %>%
+  stac_search(collections = "io-lulc-annual-v02",
+              bbox = c(kgridbox[1], kgridbox[3], kgridbox[2], kgridbox[4]),
+              datetime = "2019-01-01/2019-12-31",
+              limit = 1000) %>%
+  get_request() %>%
+  items_sign(sign_fn = sign_planetary_computer())
+# many things!
+it_obj
+# let's just download the first one
+url <- paste0("/vsicurl/", it_obj$features[[1]]$assets$data$href)
+# also get the bounding box for that area
+bb <- it_obj$features[[1]]$bbox
+# load raster
+rall <- rast(url)
+# keep just the layer we want and transform to array
+rall <- as.array(rall[["52S_20190101-20200101"]])
+# now back to raster with the appropriate extent and CRS
+rall <- rast(rall, crs = "EPSG:4326", extent = ext(c(bb[1], bb[3], bb[2], bb[4])))
+
+
+
+
 
 
